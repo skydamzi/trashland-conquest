9a9af91bc1b0f6e2f1ab992342d2003aba734072
zrk-jw <elbesmail@gmail.com>
2025-08-05 14:27:26 +0900

    타이틀로 이동하기 추가+개선
    esc - 설정 - 타이틀로 이동하기로 타이틀로 이동가능
    |새 기능들이 프리팹에서 올바르게 활용되지 못했기때문에 되도록 수정

--- Trashland Conquest/Assets/Scripts/Pause/PauseMenu.cs
+++ Trashland Conquest/Assets/Scripts/Pause/PauseMenu.cs
@@ -1,14 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
-    [Header("패널 연결")]
+    [Header("메인 패널 연결")]
     public GameObject equipmentPanel;
     public GameObject inventoryPanel;
     public GameObject skillPanel;
     public GameObject settingsPanel;
+
+    [Header("부가 패널 연결")]
+    public GameObject confirmExitPanel;
 
     private Dictionary<string, GameObject> panels = new Dictionary<string, GameObject>();
 
@@ -40,5 +44,45 @@ public class PauseMenu : MonoBehaviour
 
             Debug.Log($"패널 변경: {panelName} → 인덱스 {Pause.Instance.currentPanelIndex}");
         }
+    }
+    // ✅ 추가 : Setting Panel의 '타이틀로' 버튼에 연결
+    public void ShowExitPanel()
+    {
+        Debug.Log("타이틀로 나가기  버튼을 눌렀습니다.");
+        confirmExitPanel.SetActive(true);
+        
+    }
+
+    // ✅ 추가 : Yes 버튼에 연결
+    public void OnConfirmYes()
+    {
+        Debug.Log("타이틀로 이동합니다.");
+
+        
+        StartCoroutine(ExitWithFade());
+        if (confirmExitPanel != null)
+            confirmExitPanel.SetActive(false);
+    }
+
+    // ✅ 추가 : No 버튼에 연결
+    public void OnConfirmNo()
+    {
+        if (confirmExitPanel != null)
+            confirmExitPanel.SetActive(false);
+    }
+
+    // xkdlxmf타이틀 넘어갈때 함수로 fadeout-추가할것
+    // pause창 닫기게도 위에거추가할때같이수정
+    IEnumerator ExitWithFade()
+    {
+        // fadePanel을 만들었다면 여기에 연결 (아직 안 만들었으면 주석처리 가능)
+        // fadePanel.SetActive(true);   
+
+        yield return new WaitForSecondsRealtime(0.5f);
+        if (Pause.Instance != null)
+        {
+            Pause.Instance.TogglePause();
+        }
+        SceneManager.LoadScene("Title");
     }
 }
